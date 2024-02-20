54b9123ba2a3662e075b33adc948967bfe2c1aaf
hozayves <muhozayves2014@gmail.com>
2024-02-20 22:37:31 +0200

    fx-delete-unnecessary-code

--- js/validate-contactme.js
+++ js/validate-contactme.js
@@ -32,7 +32,6 @@ function validateEmail(input, requiredMsg, invalidMsg) {
     const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     
     const email = input.value.trim();
-    const words = email.split(/\s+/);
     if (!emailRegex.test(email)) {
         return showError(input, invalidMsg)
     }
